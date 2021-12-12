7ff421e242d30db30c74cc264a3649269a019f6f
sean-udayantha <seanudayantha@mail.com>
2021-12-12 14:27:34 +0530

    first commit

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private float horizontalnput;
-
+    //first commit
     // Start is called before the first frame update
     void Start()
     {
